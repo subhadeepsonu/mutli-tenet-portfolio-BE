0b773e8d3e877dd0448122c865b2c266b354f4bf
SONU <122858100+subhadeepsonu@users.noreply.github.com>
2025-04-02 19:51:57 +0530

    mail location changed

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -11,7 +11,7 @@ export async function Mail(to: string, subject: string, token: string) {
             html: `
             <h1>Welcome to Our Platform</h1>
             <p>Thank you for signing up! 🎉</p>
-            <a href="https://hivefolio.xyzverify/${token}" style="display: inline-block; padding: 10px 20px; color: white; background-color: #007BFF; text-decoration: none; border-radius: 5px;">Verify Email</a>
+            <a href="https://hivefolio.xyz/verify/${token}" style="display: inline-block; padding: 10px 20px; color: white; background-color: #007BFF; text-decoration: none; border-radius: 5px;">Verify Email</a>
           `,
         });
         if (error) {
